07f1c58015023828765dfc8f543e78e32b1b3fd7
PauloAlves8039 <pj38alves@gmail.com>
2024-03-21 20:06:01 -0300

    feat: criação de lógica para números romanos

--- src/Dotnet.NumerosRomanos/Program.cs
+++ src/Dotnet.NumerosRomanos/Program.cs
@@ -1,8 +1,17 @@
-﻿internal class Program
+﻿using Dotnet.NumerosRomanos;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("********** Conversor de Número Romano **********");
+
+        var numeroRomano = new ConversorDeNumeroRomano();
+        var valorRecebido = "III";
+
+        var resultado = numeroRomano.ConverteNumero(valorRecebido);
+
+        Console.WriteLine($"O valor '{valorRecebido}' foi convertido para o número: {resultado}");
 
         Console.ReadLine();
     }
