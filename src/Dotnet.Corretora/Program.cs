778d0db85edf97e35f47b82aa79b05711c6657a4
PauloAlves8039 <pj38alves@gmail.com>
2024-03-28 18:44:06 -0300

    feat: adição de projeto console para corretora de imóveis

---
+++ src/Dotnet.Corretora/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Corretora de Imóveis **********");
+
+        Console.ReadKey();
+    }
+}
