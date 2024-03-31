89ba589116006ca2b64c760c6ec8e621aab84326
PauloAlves8039 <pj38alves@gmail.com>
2024-03-31 19:18:10 -0300

    feat: aplicação de lógica para o projeto formas geométricas

---
+++ src/Dotnet.FormasGeometricas/Utils/Circulo.cs
@@ -0,0 +1,26 @@
+﻿using Dotnet.FormasGeometricas.Interfaces;
+
+namespace Dotnet.FormasGeometricas.Utils
+{
+    public class Circulo : IFormaGeometrica
+    {
+        public double Raio { get; set; }
+
+        public Circulo(double raio)
+        {
+            Raio = raio;
+        }
+
+        public double CalcularArea()
+        {
+            var resultadoArea = Math.PI * Math.Pow(Raio, 2);
+            return resultadoArea;
+        }
+
+        public double CalcularPerimetro()
+        {
+            var resultadoPerimetro = 2 * Math.PI * Raio;
+            return resultadoPerimetro;
+        }
+    }
+}
---
+++ src/Dotnet.FormasGeometricas/Utils/Retangulo.cs
@@ -0,0 +1,28 @@
+﻿using Dotnet.FormasGeometricas.Interfaces;
+
+namespace Dotnet.FormasGeometricas.Utils
+{
+    public class Retangulo : IFormaGeometrica
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public Retangulo(double largura, double altura)
+        {
+            Largura = largura;
+            Altura = altura;
+        }
+
+        public double CalcularArea()
+        {
+            var resultadoArea = Largura * Altura;
+            return resultadoArea;
+        }
+
+        public double CalcularPerimetro()
+        {
+            var resultadoPerimetro = 2 * (Largura + Altura);
+            return resultadoPerimetro;
+        }
+    }
+}
