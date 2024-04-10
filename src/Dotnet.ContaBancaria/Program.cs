339a0c66b54d3c5371cbcb2797e9ed2a9d52d536
PauloAlves8039 <pj38alves@gmail.com>
2024-04-10 20:40:37 -0300

    feat: criação de lógica para conta bancária

--- src/Dotnet.ContaBancaria/Program.cs
+++ src/Dotnet.ContaBancaria/Program.cs
@@ -1,8 +1,46 @@
-﻿internal class Program
+﻿using Dotnet.ContaBancaria.Utils;
+using System.Globalization;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("********** Conta Bancária **********");
+
+        Console.WriteLine("\nDigite as operações para a Conta Corrente: ");
+
+        Console.Write("Valor do depósito: ");
+        double depositoContaCorrente = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.Write("Valor do saque: ");
+        double saqueContaCorrente = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("\nDigite as operações para a Conta Poupança: ");
+
+        Console.Write("Valor do depósito: ");
+        double depositoContaPoupanca = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.Write("Valor do saque: ");
+        double saqueContaPoupanca = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        ContaCorrente contaCorrente = new ContaCorrente(1000);
+        ContaPoupanca contaPoupanca = new ContaPoupanca(1000);
+
+        Console.WriteLine("\n---------- Resultado Conta Corrente ----------");
+
+        Console.WriteLine("\nSaldo Inicial da Conta Corrente: " + contaCorrente.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+        contaCorrente.Depositar(depositoContaCorrente);
+        Console.WriteLine("Saldo após depósito na Conta Corrente: " + contaCorrente.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+        contaCorrente.Sacar(saqueContaCorrente);
+        Console.WriteLine("Saldo após saque na Conta Corrente: " + contaCorrente.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.WriteLine("\n---------- Resultado Conta Poupança ----------");
+
+        Console.WriteLine("\nSaldo Inicial da Conta Poupança: " + contaPoupanca.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+        contaPoupanca.Depositar(depositoContaPoupanca);
+        Console.WriteLine("Saldo após depósito na Conta Poupança: " + contaPoupanca.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+        contaPoupanca.Sacar(saqueContaPoupanca);
+        Console.WriteLine("Saldo após saque na Conta Poupança: " + contaPoupanca.Saldo.ToString("F2", CultureInfo.InvariantCulture));
 
         Console.ReadKey();
     }
---
+++ src/Dotnet.ContaBancaria/Utils/ContaCorrente.cs
@@ -0,0 +1,37 @@
+﻿using Dotnet.ContaBancaria.Models;
+
+namespace Dotnet.ContaBancaria.Utils
+{
+    public class ContaCorrente : Conta
+    {
+        private bool _primeiroSaqueGratuito = true;
+
+        public ContaCorrente(double saldoInicial) : base(saldoInicial) {}
+
+        public override bool Sacar(double valor)
+        {
+            if (_primeiroSaqueGratuito)
+            {
+                _primeiroSaqueGratuito = false;
+            }
+            else
+            {
+                valor += 0.05;
+            }
+
+            return base.Sacar(valor);
+        }
+
+        public override void Depositar(double valor)
+        {
+            try
+            {
+                Saldo += valor;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"Erro ao depositar valor na conta corrente: {exception.Message}");
+            }
+        }
+    }
+}
